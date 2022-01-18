7fd55e8a6b1137efeb4e6a2ef00b41bc1629655e
Cristian Marin <marin101998@gmail.com>
2022-01-18 10:57:03 -0500

    +update | partial map with the component map

--- Resources/views/frontend/partials/map.blade.php
+++ Resources/views/frontend/partials/map.blade.php
@@ -1,64 +1,10 @@
-
-
 @if(isset($place->address)&&!empty($place->address))
-<div class="map bg-light">
-
+  <div class="map bg-light">
     <h3 class="text-center py-4">¿Dónde está Ubicado?</h3>
-
     <div class="content">
-        <div id="map_canvas" style="width:100%; height:314px"></div>
+      <x-isite::Maps lat="{{$place->address->lat}}" lng="{{$place->address->lng}}"
+                     locationName="{{$place->address->title}}"
+                     zoom="18"/>
     </div>
-
-</div>
+  </div>
 @endif
-
-
-@section('scripts')
-    @parent
-    <script type='text/javascript' src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.min.css" type="text/css" />
-    <script type="text/javascript">
-
-        function initialize(){
-          var map = L.map('map_canvas').setView([{{ $place->address->lat ?? '4.570868' }}, {{ $place->address->lng ?? '-74.297333' }}], 18);
-
-          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          }).addTo(map);
-
-          L.marker([{{ $place->address->lat ?? '4.570868' }}, {{ $place->address->lng ?? '-74.297333' }}]).addTo(map)
-            .bindPopup('{{ $place->address->title ?? 'Dirección' }}')
-            .openPopup();
-        }
-
-        $(document).ready(function() {
-            initialize();
-
-        });
-    </script>
-@stop
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
