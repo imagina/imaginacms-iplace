4ebfb5b472c8336fa59689c1cde639557c0954a1
Michael <msolano@imaginacolombia.com>
2022-02-11 19:09:01 -0500

    +Update: cms Translation files

--- Resources/lang/en/cms.php
+++ Resources/lang/en/cms.php
@@ -1,13 +1,23 @@
 <?php
+
 return [
-  "newPlace"  => 'New Place',
-  "updatePlace"  => 'Update Place',
-  "newCategory"  => 'New Category',
-  "updateCategory"  => 'Update Category',
-  "form"  => [
-    "fromDay"  => 'From day',
-    "untilDay"  => 'Until day',
-    "fromTime"  => 'From time',
-    "untilTime"  => 'Until time',
+  "sidebar" => [
+    "adminGroup" => "Places",
+    "adminPlaces" => "Places List",
+    "adminPlacesCreate" => "Create Place",
+    "adminPlacesUpdate" => "Update Place",
+    "adminCategories" => "Place Categories",
+    "adminSchedules" => "Schedules"
+  ],
+  "newPlace" => "New Place",
+  "updatePlace" => "Update Place",
+  "newCategory" => "New Category",
+  "updateCategory" => "Update Category",
+  "form" => [
+    "fromDay" => "From day",
+    "untilDay" => "Until day",
+    "fromTime" => "From time",
+    "untilTime" => "Until time"
   ]
 ];
+
--- Resources/lang/es/cms.php
+++ Resources/lang/es/cms.php
@@ -1,13 +1,23 @@
 <?php
+
 return [
-  "newPlace"  => 'Nuevo lugar',
-  "updatePlace"  => 'Actualizar Lugar',
-  "newCategory"  => 'Nueva Categoría',
-  "updateCategory"  => 'Actualizar Categoría',
-  "form"  => [
-    "fromDay"  => 'Desde el día',
-    "untilDay"  => 'Hasta el día',
-    "fromTime"  => 'Desde la hora',
-    "untilTime"  => 'Hasta la hora',
+  "sidebar" => [
+    "adminGroup" => "Lugares",
+    "adminPlaces" => "Lista de Lugares",
+    "adminPlacesCreate" => "Crear Lugar",
+    "adminPlacesUpdate" => "Actualizar Lugar",
+    "adminCategories" => "Categorías de Lugares",
+    "adminSchedules" => "Horarios"
+  ],
+  "newPlace" => "Nuevo lugar",
+  "updatePlace" => "Actualizar Lugar",
+  "newCategory" => "Nueva Categoría",
+  "updateCategory" => "Actualizar Categoría",
+  "form" => [
+    "fromDay" => "Desde el día",
+    "untilDay" => "Hasta el día",
+    "fromTime" => "Desde la hora",
+    "untilTime" => "Hasta la hora"
   ]
 ];
+
